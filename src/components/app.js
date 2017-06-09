82b4b002fb8b492e2364831c25068ae7dab7a8aa
Srivatsa S <srivatsashankaran@gmail.com>
2017-06-09 23:18:57 +0530

    Added the app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    recentCampers: [],
+    allTimeCampers: [],
+    currentView: 'recentCampers'
+  };
+}
+
+componentWillMount(){
+  //make concurrent requests and set state to response
+  axios.all([this.fetchRecentCampers(),this.fetchallTimeCampers()])
+    .then(axios.spread(function(recentCampers , allTimeCampers){
+      this.setState({
+        recentCampers,
+        allTimeCampers
+      })
+    }));
+}
+
+fetchRecentCampers(){
+  return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+}
+
+fetchallTimeCampers(){
+  return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+}
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h2>Displaying Campers</h2>
+        <button className="btn btn-primary">Recent</button>
+        <button className="btn btn-primary">All Time</button>
+      </div>
     );
   }
 }
